e7ede834adce50c53904c5a809fb2d1fc58198fd
keyminy <imanaifs@gmail.com>
2022-12-22 19:58:49 +0900

    19,vue lifecycle hook

--- test1/src/views/HomeView.vue
+++ test1/src/views/HomeView.vue
@@ -4,7 +4,7 @@
     <form action="">
       <InputField v-model="name"/>
       <br/>
-      <button>Submit</button>
+      <button @click="updateName">Submit</button>
     </form>
     {{ name }}
   </div>
@@ -18,7 +18,36 @@ export default {
   },
   data() {
     return {
-      name: ''
+      name: 'Kossie Coder'
+    }
+  },
+  beforeCreate(){
+    console.log('beforeCreate',this.name);
+  },
+  created(){
+    console.log('created',this.name);
+  },
+  beforeMount(){
+    //alert('beforeMount');
+  },
+  mounted(){
+    //alert('mounted');
+  },
+  beforeUpdate(){
+    alert('beforeUpdate');
+  },
+  updated(){
+    alert('updated');
+  },
+  beforeDestroy(){
+    alert('beforeDestory');
+  },
+  destroyed(){
+    alert('destroyed');
+  },
+  methods:{
+    updateName(){
+      this.name = "hello";
     }
   }
 }
